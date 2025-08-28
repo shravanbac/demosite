353a6ca64072dea69673b56120a71191809fa0d0
shravanbac <shravanaem12@gmail.com>
2025-08-28 15:00:37 -0400

    Update send-for-review.js

--- tools/sidekick/send-for-review.js
+++ tools/sidekick/send-for-review.js
@@ -1,8 +1,1 @@
-(() => {
-  console.log('✅ sidekick send-for-review.js loaded');
 
-  document.addEventListener('publish-for-review', () => {
-    alert('🚀 Publish For Review button clicked!');
-    console.log('✅ Event fired: publish-for-review');
-  });
-})();
