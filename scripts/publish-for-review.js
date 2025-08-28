9db312fa2f351b772145379e6a2e32e6103e4b1a
shravanbac <shravanaem12@gmail.com>
2025-08-28 15:00:12 -0400

    Update publish-for-review.js

--- scripts/publish-for-review.js
+++ scripts/publish-for-review.js
@@ -1,8 +1,10 @@
 (() => {
+  // eslint-disable-next-line no-console
   alert("📢 publish-for-review.js LOADED");
   console.log("✅ publish-for-review.js loaded");
 
   document.addEventListener("publish-for-review", () => {
+    // eslint-disable-next-line no-console
     alert("🚀 Button clicked!");
     console.log("✅ Event fired: publish-for-review");
   });
