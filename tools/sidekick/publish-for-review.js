1364e2cd7019eac3c3d9cdfa0871737837933d88
shravanbac <shravanaem12@gmail.com>
2025-08-28 20:53:15 -0400

    Create publish-for-review.js

---
+++ tools/sidekick/publish-for-review.js
@@ -0,0 +1,32 @@
+(() => {
+  document.addEventListener('publish-for-review', async () => {
+    const payload = {
+      url: window.location.href,
+      title: document.title,
+      submittedBy: 'shravan',
+      isoNow: new Date().toISOString(),
+    };
+
+    try {
+      const res = await fetch(
+        'https://hook.app.workfrontfusion.com/3o5lrlkstfbbrspi35hh0y3cmjkk4gdd',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload),
+        },
+      );
+
+      if (res.ok) {
+        // eslint-disable-next-line no-alert
+        alert('✅ Page sent to Workfront Fusion successfully');
+      } else {
+        // eslint-disable-next-line no-alert
+        alert('❌ Workfront Fusion webhook failed: ');
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-alert
+      alert('❌ Error sending webhook: ');
+    }
+  });
+})();
